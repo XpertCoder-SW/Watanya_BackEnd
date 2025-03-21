2fe12b24dd828c247ebdb403513215f3786c58c3
MohamedEMAM388 <MohamedEmam2302301@gmail.com>
2025-03-21 23:21:05 +0200

    make store function to doctor,grades,student,subject controller

--- app/Http/Controllers/DoctorsController.php
+++ app/Http/Controllers/DoctorsController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Doctor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class DoctorsController
+class DoctorsController 
 {
     // Get All Doctors to be shown in the first page of Doctors
     public function index()
@@ -13,5 +14,36 @@ class DoctorsController
         $doctors = Doctor::all(['name', 'email', 'phoneNumber']);
 
         return response()->json(['doctors' => $doctors], 200);
+    }
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:doctors,email',
+            'phoneNumber' => 'required|string|max:255',
+            'subject_id' => 'required|exists:subjects,id',
+            'code' => 'required|string|max:255|unique:doctors,code',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $doctor = Doctor::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'phoneNumber' => $validated['phoneNumber'],
+            'code' => $validated['code'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        // Linking the doctor to the subject
+        $doctor->subjects()->attach($validated['subject_id']);
+
+        return response()->json(['message' => 'Doctor added successfully', 'doctor' => $doctor], 201);
+    }
+
+
+    public function update(Request $request , $id){
+
     }
 }
--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class StudentsController
+class StudentsController 
 {
     // Get All Students to be shown in the first page of Students with filteration
     public function index(Request $request)
@@ -20,5 +21,42 @@ class StudentsController
 
         $students = $query->get(['name', 'email', 'phoneNumber', 'level', 'specialization']);
         return response()->json(['students' => $students], 200);
+    }
+
+
+    public function store(Request $request)
+    {
+
+        $validated = $request->validate([
+            'code' => 'required|string|max:100|unique:students,code',
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|unique:students,email',
+            'phoneNumber' => 'required|string|max:100',
+            'level' => 'required|in:One,Two,Three,Four',
+            'specialization' => 'required|in:CS,IT',
+        ]);
+
+        $student = Student::create([
+            'code' => $validated['code'],
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'phoneNumber' => $validated['phoneNumber'],
+            'level' => $validated['level'],
+            'specialization' => $validated['specialization'],
+            'password' => Hash::make($validated['code']), // The default password is code.
+            'academic_year' => '2023-2024', // It can be dynamic
+            'gpa' => 0.00, // Default value because the student is new and has not received grades yet.
+        ]);
+
+        return response()->json([
+            "message" => "Student added successfully",
+            "student" => "$student",
+        ], 200);
+    }
+
+
+
+    public function update(Request $request , $id){
+
     }
 }
